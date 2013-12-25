a4eaf37727e94276d567a091563e0d475e6aba4f
benjamin_bergia <benjamin.bergia@gmail.com>
2013-12-25 14:42:46 +0800

    Add test function to view_sup

--- view_sup.erl
+++ view_sup.erl
@@ -2,15 +2,17 @@
 -behaviour(supervisor).
 
 -export([start_link/0, init/1]).
+-export([update_firstview/1]).
 
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init(_) ->
-	StateData = {first, {weighted, 1}, dead, [{local, secondview}], []},
+	StateData = {first, {weighted, 1}, dead, [{local, secondview}], [{view_sup, dead, 1}]},
 	FirstView = {firstview, {view_fsm, start_link, [{local, firstview}, StateData]}, permanent, 2000, worker,[view_fsm]},
 	StateData2 = {second, {weighted, 1}, dead, [], [{{local, firstview}, dead, 1}]},
 	SecondView = {secondview, {view_fsm, start_link, [{local, secondview}, StateData2]}, permanent, 2000, worker,[view_fsm]},
 	{ok, {{one_for_one,1 ,1},[FirstView, SecondView]}}.
 
-
+update_firstview(State) ->
+	gen_fsm:send_event({local, firstview}, {view_sup, State}).
