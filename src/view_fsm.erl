e57e72f64b4c9582922ab9e33c01ba243a73d7e4
benjamin_bergia <benjamin.bergia@gmail.com>
2013-12-26 12:48:57 +0800

    Revert "Added macro"
    This reverts commit 89a9169af07df8b431ce9f3bb02e2ce804593634.

--- src/view_fsm.erl
+++ src/view_fsm.erl
@@ -2,9 +2,7 @@
 -behaviour(gen_fsm).
 
 -export([start_link/2, init/1, alive/2, dead/2, suspicious/2]).
--ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
--endif.
 
 start_link({Scope, Name}, StateData) ->
 	gen_fsm:start_link({Scope, Name}, view_fsm, StateData, []).
@@ -56,11 +54,9 @@ one_for_all_engine(State, {_, ViewState}) ->
 		false ->
 			State
 	end.
--ifdef(TEST).
 one_for_all_engine_test() ->
 	dead = one_for_all_engine(alive, {"view", dead}),
 	alive = one_for_all_engine(alive, {"view", alive}).
--endif.
 %%
 
 %% all_for_one: State changes when all the lower Views have the same State
@@ -77,13 +73,11 @@ all_for_one_engine(State, LowerViews) ->
 		false ->
 			State
 	end.
--ifdef(TEST).
 all_for_one_engine_test() ->
 	LowerViews = [{one, alive, 1}, {two, dead, 2}, {three, alive, 1}],
 	LowerViews2 = [{one, alive, 1}, {two, alive, 3}, {three, alive, 1}],
 	dead = all_for_one_engine(dead, LowerViews),
 	alive = all_for_one_engine(dead, LowerViews2).
--endif.
 %%
 
 %% weighted State change when the weight sum of the lower views sharing a same state is equal or greater than the threshold value
@@ -104,60 +98,50 @@ weighted_engine(Threshold, State, LowerViews) ->
 		Sum >= Threshold -> 
 			NewState
 	end.
--ifdef(TEST).
 weighted_engine_test() ->
 	LowerViews = [{one, alive, 1}, {two, dead, 2}, {three, alive, 1}],
 	LowerViews2 = [{one, alive, 1}, {two, dead, 3}, {three, alive, 1}],
 	suspicious = weighted_engine(2, alive, LowerViews),
 	alive = weighted_engine(5, alive, LowerViews),
 	dead = weighted_engine(1, alive, LowerViews2).
--endif.
 %%
 
 %% Update the state of a specific View in the LowerViews list
 update_lowerviews_state({View, ViewState}, LowerViews) -> 
 	lists:keyreplace(View, 1, LowerViews, {View, ViewState, get_view_weight(View, LowerViews)}).
--ifdef(TEST).
 update_lowerviews_state_test() ->
 	LowerViews = [{"first", alive, 1}, {"myview", dead, 2}, {myview, alive, 1}],
 	Result = [{"first", alive, 1}, {"myview", suscpicious, 2}, {myview, alive, 1}],
 	Result = update_lowerviews_state({"myview", suscpicious}, LowerViews).
--endif.
 %%
 
 %% Update the weight of a specific View in the LowerViews list
 update_lowerviews_weight({View, Weight}, LowerViews) ->
 	lists:keyreplace(View, 1, LowerViews, {View, get_view_state(View, LowerViews), Weight}).
--ifdef(TEST).
 update_lowerviews_weight_test() ->
 	LowerViews = [{"first", alive, 1}, {"myview", dead, 2}, {myview, alive, 1}],
 	Result = [{"first", alive, 1}, {"myview", dead, 3}, {myview, alive, 1}],
 	Result = update_lowerviews_weight({"myview", 3}, LowerViews).
--endif.
 %%
 
 %% Get the weight of a View in the LowerViews list 
 get_view_weight(View, LowerViews) ->
 	{_, _, Weight} = lists:keyfind(View, 1, LowerViews),
 	Weight.
--ifdef(TEST).
 get_view_weight_test() ->
 	LowerViews = [{"first", alive, 1}, {"myview", dead, 2}, {myview, alive, 1}],
 	View = "myview",
 	2 = get_view_weight(View, LowerViews).
--endif.
 %%
 
 %% Get the state of a View in the LowerViews list
 get_view_state(View, LowerViews) ->
 	{_, State, _} = lists:keyfind(View, 1, LowerViews),
 	State.
--ifdef(TEST).
 get_view_state_test() ->
 	LowerViews = [{"first", alive, 1}, {"myview", dead, 2}, {myview, alive, 1}],
 	View = "myview",
 	dead = get_view_state(View, LowerViews).
--endif.
 %%
 
 %% Insertion sort on the second elements of a Tuple List 
@@ -169,12 +153,10 @@ inverted_insertion(Acc={_, AccSecond}, List=[{_, Second}|_]) when AccSecond >= S
 	[Acc|List];
 inverted_insertion(Acc,[H|T]) ->
 	[H|inverted_insertion(Acc, T)].
--ifdef(TEST).
 inverted_insertion_sort_test() ->
 	Unsorted = [{3, 5}, {2, 6}, {1, 7}, {5, 1}],
 	SortedSecond = [{1, 7}, {2, 6}, {3, 5}, {5, 1}],
 	SortedSecond = inverted_insertion_sort(Unsorted).
--endif.
 %%
 
 %% Sum the weight of all the Views of a give State
@@ -185,12 +167,10 @@ sum_weights(State, ViewList) ->
 				WeightSum
 		end,
 	{State, lists:foldl(Sum, 0, ViewList)}.
--ifdef(TEST).
 sum_weights_test() ->
 	Views = [{first, alive, 1}, {second, alive, 3}, {third, suspicious, 2}, {fourth, dead, 2}],
 	{alive, 4} = sum_weights(alive, Views),
 	{dead, 2} = sum_weights(dead, Views).
--endif.
 %%
 
 %% Lists all the different State present inside the LowerViews list
@@ -198,9 +178,7 @@ list_states(Views) ->
 	{_, States, _} = lists:unzip3(Views),
 	%%sets:to_list(sets:from_list(States)).
 	lists:usort(States).
--ifdef(TEST).
 list_states_test() ->
 	Views = [{first, alive, 1}, {second, alive, 3}, {third, suspicious, 2},     {fourth, dead, 2}],
 	[alive, dead, suspicious] = list_states(Views).
--endif.
 %%
