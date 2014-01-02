3f43e060ec6c7a1f84b47b52f2367590b026713f
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-02 10:35:46 +0800

    commit all

--- src/config_parser.erl
+++ src/config_parser.erl
@@ -1,8 +1,6 @@
--module(state_store).
+-module(config_parser).
 -behaviour(gen_server).
--include("state.hrl").
 -define(SERVER, ?MODULE).
--define(DB, "/tmp/mnesia").
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -17,8 +15,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([start/0, stop/0, install/0, set_state/1, get_state/1]).
-
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -31,17 +27,10 @@ start_link() ->
 %% ------------------------------------------------------------------
 
 init(Args) ->
-	start(),
-    	{ok, Args}.
+    {ok, Args}.
 
-handle_call(Request, _From, State) ->
-	case Request of
-		{set, ViewState} ->
-			set_state(ViewState);
-		{get, ViewName} ->
-			get_state(ViewName)
-	end,
-    	{reply, ok, State}.
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -50,8 +39,7 @@ handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, _State) ->
-	stop(),
-    	ok.
+    ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -59,30 +47,4 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
-
-start() ->
-	application:set_env(mnesia, dir, ?DB),
-	application:start(mnesia).
-
-stop() ->
-	application:stop(mnesia).
- 
-install() ->
-	application:set_env(mnesia, dir, ?DB),
-	mnesia:create_schema([node()]),
-	application:start(mnesia),
-	%%mnesia:create_table(state, [{disc_copies, [node()]}, {ram_copies, [node()]}, {type, set}, {attributes, record_info(fields, state)}, {index, [view_name]}]),
-	mnesia:create_table(state, [{attributes, record_info(fields, state)}, {disc_copies ,nodes()}]).
-
-set_state(State) ->
-	Write = fun() ->
-			mnesia:write(State)
-		end,
-	mnesia:transaction(Write).
-
-get_state(ViewName) ->
-	Read = fun() ->
-			mnesia:read({state, ViewName})
-		end,
-	mnesia:transaction(Read).
 
