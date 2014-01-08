d86e2958cf3e054c28d8c04f852e73bc516105f8
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-08 22:51:31 +0800

    Switch view_core to a gen_fsm

--- src/view_core.erl
+++ src/view_core.erl
@@ -1,5 +1,5 @@
 -module(view_core).
--behaviour(gen_server).
+-behaviour(gen_fsm).
 -define(SERVER, ?MODULE).
 
 %% ------------------------------------------------------------------
@@ -9,40 +9,47 @@
 -export([start_link/0]).
 
 %% ------------------------------------------------------------------
-%% gen_server Function Exports
+%% gen_fsm Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, state_name/2, state_name/3, handle_event/3,
+         handle_sync_event/4, handle_info/3, terminate/3,
+         code_change/4]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 %% ------------------------------------------------------------------
-%% gen_server Function Definitions
+%% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init(_Args) ->
+    {ok, initial_state_name, initial_state}.
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+state_name(_Event, State) ->
+    {next_state, state_name, State}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+state_name(_Event, _From, State) ->
+    {reply, ok, state_name, State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_event(_Event, StateName, State) ->
+    {next_state, StateName, State}.
 
-terminate(_Reason, _State) ->
+handle_sync_event(_Event, _From, StateName, State) ->
+    {reply, ok, StateName, State}.
+
+handle_info(_Info, StateName, State) ->
+    {next_state, StateName, State}.
+
+terminate(_Reason, _StateName, _State) ->
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
