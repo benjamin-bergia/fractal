648963dcc51737754c0ce78b12893a7ce22864ed
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-08 17:33:57 +0800

    added the basic features to the rx module

--- src/view_rx.erl
+++ src/view_rx.erl
@@ -2,49 +2,53 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
+-record(state, {accumulator, subscriptions}).
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_call/3, terminate/2]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Subscriptions) ->
+	S = #state{subscriptions=Subscriptions},
+	gen_server:start_link({local, ?SERVER}, ?MODULE, S, []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init(S) ->
+	subscribe(S#state.subscriptions),	
+	{ok, S}.
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_call({status_change, _View, _Status}=Msg, _From, S) ->
+	forward(S#state.accumulator, Msg),
+	{reply, ok, S}.
 
 terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+	ok.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+subscribe(Names) ->
+	F = fun(Name, Acc) ->
+			[{Name, sub}|Acc]
+		end,
+	Subscriptions = lists:foldl(F, [], Names),
+	gproc:mreg(p, l, Subscriptions).
+
+forward(To, Msg) ->
+	gen_server:call(To, Msg).
