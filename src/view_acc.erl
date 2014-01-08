dea2bd5bf53b417de05bd5daa95cac9a946e040f
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-08 18:11:47 +0800

    Started to work on the accumulator

--- src/view_acc.erl
+++ src/view_acc.erl
@@ -3,49 +3,39 @@
 -define(SERVER, ?MODULE).
 
 -record(state, {name, deads=[], alives=[], suspiciouses=[]}).
--record
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_call/3, terminate/2]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Name) ->
+	S = #state{name=Name},
+	gen_server:start_link({local, ?SERVER}, ?MODULE, S, []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init(S) ->
+	{ok, S}.
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info(_Info, State) ->
-    {noreply, State}.
+	{reply, ok, State}.
 
 terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+	ok.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
