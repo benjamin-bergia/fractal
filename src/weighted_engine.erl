05119a3d253af9cdceec088068692532dca0cfe0
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-08 23:57:55 +0800

    Added core functions

--- src/weighted_engine.erl
+++ src/weighted_engine.erl
@@ -2,5 +2,15 @@
 
 -include("engine_api.hrl").
 
-start(Engine, Threshold, DeadSum, AliveSum, SuspiciousSum) ->
+start(Threshold, DeadSum, AliveSum, SuspiciousSum) ->
+	[{Status, Sum}|[{SecondStatus, SecondSum}|_T]] = inverted_insertion_sort([{dead, DeadSum}, {alive, AliveSum}, {suspicious, SuspiciousSum}]),
 	true.
+
+inverted_insertion_sort(List) ->
+	        lists:foldl(fun inverted_insertion/2, [], List).
+inverted_insertion(Acc, []) ->
+	        [Acc];
+inverted_insertion(Acc={_, AccSecond}, List=[{_, Second}|_]) when AccSecond >= Second ->
+	        [Acc|List];
+inverted_insertion(Acc,[H|T]) ->
+	        [H|inverted_insertion(Acc, T)].
