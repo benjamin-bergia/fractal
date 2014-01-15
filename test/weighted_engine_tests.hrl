e8646f8a0c4232ec6d1331b2fe90254d01e371e6
benjamin_bergia <benjamin.bergia@gmail.com>
2014-01-15 22:59:47 +0800

    Updated unit tests

--- test/weighted_engine_tests.hrl
+++ test/weighted_engine_tests.hrl
@@ -1,10 +1,11 @@
 -include_lib("eunit/include/eunit.hrl").
+-define(STATUSLIST(D, A, S), [{dead, D}, {alive, A}, {suspicious, S}]).
 
-start_test() ->
-	?assertEqual(alive, start(dead, 5, 3, 6, 0)),
-	?assertEqual(dead, start(dead, 5, 3, 4, 0)),
-	?assertEqual(suspicious, start(dead, 5, 3, 6, 6)),
-	?assertEqual(dead, start(dead, 5, 3, 3, 3)).
+init_test() ->
+	?assertEqual({stop, alive}, init(dead, 5, ?STATUSLIST(3, 6, 0))),
+	?assertEqual({stop, dead}, init(dead, 5, ?STATUSLIST(3, 4, 0))),
+	?assertEqual({stop, suspicious}, init(dead, 5, ?STATUSLIST(3, 6, 6))),
+	?assertEqual({stop, dead}, init(dead, 5, ?STATUSLIST(3, 3, 3))).
 
 inverted_insertion_sort_test() ->
 	List = [{0, 1}, {a, 5}, {"test", 3}],
