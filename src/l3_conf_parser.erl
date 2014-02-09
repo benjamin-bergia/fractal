d04e0e0ecb128291ae9d8022c97e588d01c60ab7
benjamin_bergia <benjamin.bergia@gmail.com>
2014-02-09 20:52:34 +0800

    One more test

--- src/l3_conf_parser.erl
+++ src/l3_conf_parser.erl
@@ -1,6 +1,10 @@
 -module(l3_conf_parser).
 -export([parse/1]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 parse(FilePath) ->
 	[break(Conf) || Conf <- get_conf(FilePath)].
 
@@ -16,15 +20,62 @@ break({view, {id, ViewID}, {name, _ViewName}, StatusA, StatusB, StatusC}) ->
 	{AE, AT, AL} = get_status(StatusB),
 	{SE, ST, SL} = get_status(StatusC),
 	[ViewID, DE, DT, DL, AE, AT, AL, SE, ST, SL].
+-ifdef(TEST).
+break_test_() ->
+	SimpleView = {view,
+		      {id, 1},
+		      {name, "nothing"},
+		      {all,
+		       {engine, engine},
+		       {threshold, 2},
+		       {lowers, [{lower,
+				  {id, 2},
+				  {weight, 3}}]}}},
+	ComplexView = {view,
+		      {id, 1},
+		      {name, "nothing"},
+		      {dead,
+		       {engine, engine},
+		       {threshold, 2},
+		       {lowers, [{lower,
+				  {id, 2},
+				  {weight, 3}}]}},
+		      {alive,
+		       {engine, engine},
+		       {threshold, 2},
+		       {lowers, []}},
+		      {suspicious,
+		       {engine, engine},
+		       {threshold, 2},
+		       {lowers, [{lower,
+				  {id, 2},
+				  {weight, 3}}]}}},
 
-get_status({all, {engine, Engine}, {threshold, Threshold}, Lowers}) ->
-	{Engine, Threshold, get_lowers(Lowers)};
-get_status({dead, {engine, Engine}, {threshold, Threshold}, Lowers}) ->
-	{Engine, Threshold, get_lowers(Lowers)};
-get_status({alive, {engine, Engine}, {threshold, Threshold}, Lowers}) ->
-	{Engine, Threshold, get_lowers(Lowers)};
-get_status({suspicious, {engine, Engine}, {threshold, Threshold}, Lowers}) ->
+	[?_assertEqual([1, engine, 2, [{2, 3}]],
+		       break(SimpleView)),
+	 ?_assertEqual([1, engine, 2, [{2, 3}],
+				engine, 2, [],
+				engine, 2, [{2, 3}]],
+		       break(ComplexView))].
+-endif.
+
+get_status({all, _Engine, _Threshold, _Lowers}=Data) ->
+	get_status_safe(Data);
+get_status({dead, _Engine, _Threshold, _Lowers}=Data) ->
+	get_status_safe(Data);
+get_status({alive, _Engine, _Threshold, _Lowers}=Data) ->
+	get_status_safe(Data);
+get_status({suspicious, _Engine, _Threshold, _Lowers}=Data) ->
+	get_status_safe(Data).
+get_status_safe({_Status, {engine, Engine}, {threshold, Threshold}, Lowers}) ->
 	{Engine, Threshold, get_lowers(Lowers)}.
+-ifdef(TEST).
+get_status_safe_test_() ->
+	Input = {dead, {engine, engine},
+			{threshold, 2},
+			{lowers, [{lower, {id, 1}, {weight, 1}}]}},
+	?_assertEqual({engine, 2, [{1, 1}]}, get_status(Input)).
+-endif.
 
 get_lowers({lowers, []}) ->
 	[];
@@ -33,3 +84,7 @@ get_lowers({lowers, LowersList}) ->
 			[{ID, Weight}|Acc]
 		end,
 	lists:foldl(F, [], LowersList).
+-ifdef(TEST).
+get_lowers_test_() ->
+	?_assertEqual([{3, 4}, {1, 2}], get_lowers({lowers, [{lower, {id, 1}, {weight, 2}}, {lower, {id, 3}, {weight, 4}}]})).
+-endif.
