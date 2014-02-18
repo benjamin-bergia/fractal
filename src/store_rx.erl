b4ac17ad87ed1bb9c202a6993c4569fa5d158d55
benjamin_bergia <benjamin.bergia@gmail.com>
2014-02-18 13:50:00 +0800

    Added a function to query all the views

--- src/store_rx.erl
+++ src/store_rx.erl
@@ -29,6 +29,8 @@ start_link() ->
 set_status(ViewID, Status) ->
 	gen_server:cast(?MODULE, {set_status, ViewID, Status}).
 
+get_status(all) ->
+	gen_server:call(?MODULE, {get_status, all});
 get_status(ViewID) ->
 	gen_server:call(?MODULE, {get_status, ViewID}).
 
@@ -43,6 +45,8 @@ handle_cast({set_status, ViewID, Status}, S) ->
 	write(#view_status{view_id=ViewID, status=Status}),
 	{noreply, S}.
 
+handle_call({get_status, all}, _From, S) ->
+	{reply, select_all(), S};
 handle_call({get_status, ViewID}, _From, S) ->
 	{reply, read(ViewID), S}.
 
@@ -64,6 +68,15 @@ read(ViewID) ->
 			mnesia:read({view_status, ViewID})
 		end,
 	mnesia:activity(sync_transaction, Read).
+
+select_all() ->
+	MatchHead = '_',
+	Guard = [],
+	Result = ['$$'],
+	Select = fun() ->
+			mnesia:match_object(view_status, {view_status, '_', '_'}, read)
+		end,
+	mnesia:activity(sync_transaction, Select).
 
 install() ->
 	ok = mnesia:create_schema([node()]),
